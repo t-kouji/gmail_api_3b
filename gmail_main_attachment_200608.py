72896031f22a88e637dcef0569f7a81ca982d41f
kouji tanaka <t.kouji1281@gmail.com>
2020-06-18 23:12:21 +0900

    gmail-apiで添付ファイル振り分けまで完了

--- gmail_main_attachment_200608.py
+++ gmail_main_attachment_200608.py
@@ -36,37 +36,24 @@ service = gmail_get_service()
 messages = service.users().messages()
 # 自分のメッセージ一覧を"maxResults"件得る
 number_of_requests = int(input("取得したいメールの件数>"))
-msg_list = messages.list(userId='me', maxResults=number_of_requests).execute()
-# pprint(msg_list)
-#上記のメッセージID一覧を取得（idとthreadIdをリスト内の辞書として）
-msg_ID_list = msg_list['messages']
-pprint(msg_ID_list)
+# msg_list = messages.list(userId='me', maxResults=number_of_requests).execute()
+# 上記のメッセージID一覧を取得（idとthreadIdをリスト内の辞書として）
+# msg_ID_list = msg_list['messages']
 
-# def MessageDataDict(number_of_requests):
-#     """メッセージIDとメッセージ本体の辞書を作成する"""
-#     """{'id':'data'}"""
-#     message_data_dict = {}
-#     msg_list = messages.list(userId='me', maxResults=number_of_requests).execute()
-#     msg_ID_list = msg_list['messages']
-#     for msg in msg_ID_list:
-#         #メッセージIDを取得
-#         id = msg['id']
-#         # メッセージの本体を取得する
-#         data = messages.get(userId='me', id=id).execute()
-#         message_data_dict[id] = data
-#     return message_data_dict
-# MessageDataDict = MessageDataDict(number_of_requests)
-
-
-def get_dir_name():
-    """同フォルダ内のフォルダ名をリストで取得"""
-    path = "./"
-    files = os.listdir(path)
-    dir_name = [f for f in files if os.path.isdir(os.path.join(path, f))]
-    print(dir_name)
-    return dir_name
-Dir_list = get_dir_name()
-
+def MessageDataDict(number_of_requests):
+    """メッセージIDとメッセージ本体の辞書を作成する"""
+    """{'id':'data'}"""
+    message_data_dict = {}
+    msg_list = messages.list(userId='me', maxResults=number_of_requests).execute()
+    msg_ID_list = msg_list['messages']
+    for msg in msg_ID_list:
+        #メッセージIDを取得
+        id = msg['id']
+        # メッセージの本体を取得する
+        data = messages.get(userId='me', id=id).execute()
+        message_data_dict[id] = data
+    return message_data_dict
+MessageDataDict = MessageDataDict(number_of_requests)
 
 def LabelsDict(service, user_id):
     """ユーザーのメールボックスから対象となるlabelIDとラベル名の辞書を作成する"""
@@ -84,43 +71,13 @@ def LabelsDict(service, user_id):
         print("labelsdict取得する上で『{}』のエラー!".format(e))
         pass
 LabelsDict = LabelsDict(service,'me')
-pprint(LabelsDict)
+# pprint(LabelsDict)
 
-def GetAttachments(msg_ID_list):
-    """添付ファイルを取得"""
-    for msg in msg_ID_list:
-        #メッセージIDを取得
-        id = msg['id']
-        # メッセージの本体を取得する
-        data = messages.get(userId='me', id=id).execute()
-        try:
-            if data['payload']['parts'][1]['body']['attachmentId']:
-                #添付ファイルが存在するファイルに対し、attachmentIDを取得
-                attachment_ID = data['payload']['parts'][1]['body']['attachmentId']
-                # 添付ファイルの本体を取得
-                attachment = messages.attachments().get(userId='me',messageId = id, id=attachment_ID).execute()
-                # 添付ファイルのコードを変換
-                file_data = base64.urlsafe_b64decode(attachment['data'])
-                #path名＝添付ファイル名とする
-                print(f"添付ファイル名：{data['payload']['parts'][1]['filename']}")
-                path = data['payload']['parts'][1]['filename']
-                # open(path,"wb") as f: のpathはstrで./内にそのファイルが無くてもpath名で新規作成される。
-                #https://note.nkmk.me/python-file-io-open-with/ ←参考
-                with open(path,"wb") as f:
-                    f.write(file_data)
-        except KeyError: 
-            continue
-GetAttachments(msg_ID_list)
-
-def LabelId_AttachmentId(msg_ID_list):
+def LabelId_AttachmentId(MessageDataDict):
     """メッセージのlabelID、attachmentIdを取得"""
     """{'label_ID':'attachment_ID'}"""
     LabelId_AttachmentId_dict = {}
-    for msg in msg_ID_list:
-        #メッセージIDを取得
-        id = msg['id']
-        # メッセージの本体を取得する
-        data = messages.get(userId='me', id=id).execute()
+    for id,data in MessageDataDict.items():
         try:
             #メッセージ本体に"labalIds"キーが存在した場合、
             if data['labelIds']:
@@ -134,8 +91,8 @@ def LabelId_AttachmentId(msg_ID_list):
             print("LabelId_AttachmentId_dict取得する上で『{}』のエラー!".format(e))
             pass
     return LabelId_AttachmentId_dict
-LabelId_AttachmentId = LabelId_AttachmentId(msg_ID_list)
-pprint(LabelId_AttachmentId)
+LabelId_AttachmentId = LabelId_AttachmentId(MessageDataDict)
+# pprint(LabelId_AttachmentId)
 
 def Labelname_AttachmentId():
     """labelnameとattachmentIdの辞書を取得"""
@@ -147,13 +104,31 @@ def Labelname_AttachmentId():
                 Labelname_AttachmentId_dict[LabelsDict[labelID]] = LabelId_AttachmentId[label_ID]
     return Labelname_AttachmentId_dict
 Labelname_AttachmentId = Labelname_AttachmentId()
-pprint(Labelname_AttachmentId)
+# pprint(Labelname_AttachmentId)
 
-def Sort_Attachment():
+def Sort_Attachment(MessageDataDict):
+    """添付ファイルを各フォルダのdataフォルダ内に格納"""
     for Labelname,attachment_ID in Labelname_AttachmentId.items():
-        os.makedirs('./{}/date'.format(Labelname),exist_ok=True)
-        # 添付ファイルの本体を取得
-
-
-
-Sort_Attachment()
+        """フォルダが存在しない場合は名前を指定してフォルダを作成"""
+        os.makedirs('./projects/{}/data'.format(Labelname),exist_ok=True)
+        """添付ファイル本体の取得"""
+        try:
+            for id,data in MessageDataDict.items():
+                #該当するattachment_IDだった場合
+                if data['payload']['parts'][1]['body']['attachmentId'] == attachment_ID:
+                    """添付ファイルの本体を取得"""
+                    attachment =  messages.attachments().get(userId='me',messageId = id, id=attachment_ID).execute()
+                    """添付ファイルコードの変更"""
+                    file_data = base64.urlsafe_b64decode(attachment['data'])
+                    """添付ファイル名の取得"""
+                    file_name = data['payload']['parts'][1]['filename']
+                    print(f"添付ファイル名：{file_name}")
+                    path = './projects/{}/data/{}'.format(Labelname,file_name) 
+                    with open(path,"wb") as f:
+                        f.write(file_data)
+                    # open(path,"wb") as f: のpathはstrで./内にそのファイルが無くてもpath名で新規作成される。
+                    # https://note.nkmk.me/python-file-io-open-with/ ←参考
+        except Exception as e: 
+            print("Sort_Attachmentで添付ファイルを振り分ける上で『{}』のエラー!".format(e))
+            continue
+Sort_Attachment(MessageDataDict)
